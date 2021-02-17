e46b8a882d2cc5502b777b754104e0a9d52974c9
Francesco Michele Barranca <francescomich99@gmail.com>
2021-02-17 14:23:04 -0500

    added schedule control on admin

--- contacts/models.py
+++ contacts/models.py
@@ -8,3 +8,27 @@ class Contact(Preferences):
     address = models.CharField(max_length=100)
     facebook_link = models.CharField(max_length=300)
     instagram_link = models.CharField(max_length=300)
+
+    monday_opening_time = models.TimeField()
+    monday_closing_time = models.TimeField()
+
+    tuesday_opening_time = models.TimeField()
+    tuesday_closing_time = models.TimeField()
+
+    wednesday_opening_time = models.TimeField()
+    wednesday_closing_time = models.TimeField()
+
+    thursday_opening_time = models.TimeField()
+    thursday_closing_time = models.TimeField()
+
+    friday_opening_time = models.TimeField()
+    friday_closing_time = models.TimeField()
+
+    saturday_opening_time = models.TimeField()
+    saturday_closing_time = models.TimeField()
+
+    sunday_opening_time = models.TimeField()
+    sunday_closing_time = models.TimeField()
+
+    def __str__(self):
+        return 'Settings'
