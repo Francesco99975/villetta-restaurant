cbfc2a80eb819b5e8d48897727d880bf35f71b2c
Francesco Michele Barranca <francescomich99@gmail.com>
2021-04-12 17:29:12 -0400

    added settings

--- settings/models.py
+++ settings/models.py
@@ -5,12 +5,16 @@ from django.utils import timezone
 
 
 class Setting(Preferences):
+    website_available = models.BooleanField(default=True)
     telephone_number = models.CharField(max_length=30)
     email = models.EmailField()
     address = models.CharField(max_length=100)
     facebook_link = models.CharField(max_length=300)
     instagram_link = models.CharField(max_length=300)
     home_delivery_available = models.BooleanField(default=True)
+    home_delivery_cost = models.DecimalField(
+        max_digits=7, decimal_places=2, default=3.50)
+    order_preparation_time_in_minutes = models.IntegerField(default=30)
 
     monday_opening_time = models.TimeField(default=timezone.now)
     monday_closing_time = models.TimeField(default=timezone.now)
