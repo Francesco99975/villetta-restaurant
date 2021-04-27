588f9c8d6e485319dca61418c9dc4adeb05c4bcd
Francesco Michele Barranca <francescomich99@gmail.com>
2021-04-27 13:25:42 -0400

    fixed cors on local network

--- villetta/settings.py
+++ villetta/settings.py
@@ -167,14 +167,15 @@ CART_PRODUCT_MODEL = 'dishes.models.Dish'
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = ('http://villetta-app', 'http://villetta-app:80', 'http://localhost:80',
-                         'http://localhost', 'http://localhost:4200', 'http://192.168.0.38')
+                         'http://localhost', 'http://localhost:4200', 'http://192.168.0.38', 'http://192.168.0.38:80')
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost'
     'http://localhost:80'
     'http://localhost:4200',
     'http://villetta-app'
     'http://villetta-app:80',
-    'http://192.168.0.38'
+    'http://192.168.0.38',
+    'http://192.168.0.38:80'
 ]
 CORS_ALLOW_HEADERS = [
     'accept',
